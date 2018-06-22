fa55fe4d112156eb88cc52deec1daacd0a7a54e2
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-22 19:39:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,14 @@
 # dog.rb
 class Dog
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
- 
+
   def name
-    this_dogs_name
+    @this_dogs_name
   end
 end
 casper = Dog.new
 casper.name = "Casper"
- 
+
 puts casper.name
- 
